0a3a7d939350a8043b8351eb131edf40972fb2e9
Guðmundur Axel <gudmundur210@gmail.com>
2024-03-06 00:01:32 +0000

    Modified social icons

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,16 @@ function App() {
     <div className="App">
       <div className="container-fluid section-1">
 		<div className="row section-1-content">
-			<div className="col-xl-2 col-lg-3 offset-lg-3 align-self-center">
-				<img className="img-fluid" src="images/me.jpeg" alt="Failed"/>
+			<div className="col-xl-2 col-lg-3 col-sm-4 col-xs-12 offset-lg-1 offset-xl-2 align-self-center">
+				{/* <div className="portfolio-photo">
+				</div> */}
+				<img className="img-fluid portfolio-photo" src="images/me.jpeg" alt="Failed"/>
 			</div>
-			<div className="col-lg-5 align-self-center">
+			<div className="col-xl-6 col-lg-7 col-sm-8 align-self-center">
 				<h1>Guðmundur Axel Guðrúnarson</h1>
 				<p>
-					Hi!I have a lot of interest in Web Development and Game Development. I have a lot ofexperience in both these fields from school projects and my own personal projects.I have experience with using technologies such as React, PHP & Laravel, MySQL, SCSS.Am always willing to learn and adapt to new technologies. Also have experience with Machine Learning and Artificial Intelligence from university. I am good at collaborating within teams and equally proficient in independent work.
+					Hi!<br/>
+					I have a lot of interest in Web Development and Game Development. I have a lot ofexperience in both these fields from school projects and my own personal projects.I have experience with using technologies such as React, PHP & Laravel, MySQL, SCSS.Am always willing to learn and adapt to new technologies. Also have experience with Machine Learning and Artificial Intelligence from university. I am good at collaborating within teams and equally proficient in independent work.
 					Check out my projects below!
 				</p>
 			</div>

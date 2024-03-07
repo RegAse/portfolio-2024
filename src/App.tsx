4a87416a495d3ad49e831ca9ee5c5dbd28c76c9d
Guðmundur Axel <gudmundur210@gmail.com>
2024-03-07 22:30:27 +0000

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,112 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import { BsGithub, BsLinkedin, BsEnvelope } from "react-icons/bs";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container-fluid section-1">
-		<div className="row section-1-content">
-			<div className="col-xl-2 col-lg-3 col-sm-4 col-xs-12 offset-lg-1 offset-xl-2 align-self-center">
-				{/* <div className="portfolio-photo">
+	const bg = "../images/firefox_qmfUGQasR8.png";
+	const bg2 = "../images/Unity_RGfhouoolA.png";
+
+	useEffect(() => {
+		const startAnim = setTimeout(() => {
+			console.log("DELAYED");
+		}, 2000);
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container-fluid section-1">
+				<div className="row section-1-content">
+					<div className="col-xl-2 col-lg-3 col-sm-4 col-xs-12 offset-lg-1 offset-xl-2 align-self-center">
+						{/* <div className="portfolio-photo">
 				</div> */}
-				<img className="img-fluid portfolio-photo" src="images/me.jpeg" alt="Failed"/>
+						<img
+							className="img-fluid portfolio-photo"
+							src="images/me.jpeg"
+							alt="Failed"
+						/>
+					</div>
+					<div className="col-xl-6 col-lg-7 col-sm-8 align-self-center">
+						<h1>Guðmundur Axel Guðrúnarson</h1>
+						<p>
+							Hi!
+							<br />I have a lot of interest in Web Development and Game
+							Development. I have a lot of experience in both these fields from
+							school projects and my own personal projects.I have experience
+							with using technologies such as React, PHP & Laravel, MySQL, SCSS.
+							Always willing to learn and adapt to new technologies. Also have
+							experience with Machine Learning and Artificial Intelligence from
+							university. I am good at collaborating within teams and equally
+							proficient in independent work. Check out my projects below!
+						</p>
+					</div>
+				</div>
+				<div className="section-1-socials">
+					<BsGithub />
+					<BsLinkedin />
+					<BsEnvelope />
+				</div>
 			</div>
-			<div className="col-xl-6 col-lg-7 col-sm-8 align-self-center">
-				<h1>Guðmundur Axel Guðrúnarson</h1>
-				<p>
-					Hi!<br/>
-					I have a lot of interest in Web Development and Game Development. I have a lot ofexperience in both these fields from school projects and my own personal projects.I have experience with using technologies such as React, PHP & Laravel, MySQL, SCSS.Am always willing to learn and adapt to new technologies. Also have experience with Machine Learning and Artificial Intelligence from university. I am good at collaborating within teams and equally proficient in independent work.
-					Check out my projects below!
-				</p>
+			<div className="container section-2">
+				<div>
+					<h2 className="text-center">Education & Projects</h2>
+				</div>
+				<div className="row section-2-content">
+					<div className="col-md-5">
+						<div className="education">
+							<h4>BSc Computer Science - Háskóli Reykjavík</h4>
+							<p>
+								Learned Machine Learning and Artificical Intelligence as well as
+								C++, Game Engine Design, Web Development, Computer Graphics.
+								Technologies such as sklearn, ASP.NET, Typescript, PostgreSQL,
+								C++, Python, Java, C#, React, React Native.
+							</p>
+							<div className="education-connection-right"></div>
+							<div className="education-connection-down"></div>
+						</div>
+					</div>
+					<div className="col-md-5 offset-md-2 project-container">
+						<div
+							className="project"
+							style={{ backgroundImage: "url(" + bg + ")", animationDelay: 1*1.2 + "s"}}
+						>
+							<div className="project-info">
+								<h3>Zen Juggle</h3>
+								<p>A game I made for a game development course</p>
+							</div>
+						</div>
+						<div className="project-connection-left" style={{ animationDelay: 1*1 + "s"}}></div>
+						<div className="project-connection-left-title"  style={{ animationDelay: 1*1 + "s"}}>GCDD Course</div>
+					</div>
+					<div className="col-md-5">
+						{/* <div className="education">
+					<h3>BSc Computer Science - Háskóli Reykjavík</h3>
+					<p>Learned Machine Learning and Artificical Intelligence as well as C++, Game Engine Design, Web Development, Computer Graphics. Technologies such as sklearn, ASP.NET, Typescript, PostgreSQL, C++, Python, Java, C#, React, React Native.</p>
+					<div className="education-connection">
+
+					</div>
+				</div> */}
+					</div>
+					<div className="col-md-5 offset-md-2 mt-5 project-container">
+						<div
+							className="project"
+							style={{ backgroundImage: "url(" + bg2 + ")", animationDelay: 2*1.2 + "s"}}
+						>
+							<div className="project-info">
+								<h3>Morns</h3>
+								<p>A game I made with a group for a game development course</p>
+							</div>
+						</div>
+						<div className="project-connection-left" style={{ animationDelay: 2*1 + "s"}}></div>
+						<div className="project-connection-left-title" style={{ animationDelay: 2*1 + "s"}}>GCDD Course</div>
+					</div>
+				</div>
 			</div>
+			<button type="button" className="btn btn-primary">
+				Primary
+			</button>
 		</div>
-		<div className="section-1-socials">
-			<BsGithub />
-			<BsLinkedin />
-			<BsEnvelope />
-		</div>
-      </div>
-	  <div className="container-fluid section-2">
-	  	<div>
-			<h2 className="text-center">Education & Projects</h2>
-		</div>
-		<div className="row section-2-content">
-			<div className="col-lg-2">
-				Education & Projects
-			</div>
-		</div>
-      </div>
-      <button type="button" className="btn btn-primary">Primary</button>
-    </div>
-  );
+	);
 }
 
 export default App;

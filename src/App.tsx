c80ab8c152e1908a664c5d87ddf650b9b63d4457
Guðmundur Axel <gudmundur210@gmail.com>
2024-03-08 17:15:07 +0000

    Polish

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,28 @@ function App() {
 	return (
 		<div className="App">
 			<div className="container-fluid section-1">
-				<div className="row section-1-content">
-					<div className="col-xl-2 col-lg-3 col-sm-4 col-xs-12 offset-lg-1 offset-xl-2 align-self-center">
-						{/* <div className="portfolio-photo">
+				{/* <div className="section-1-border-top">
+
+				</div>
+				<div className="section-1-border-left">
+
 				</div> */}
-						<img
+				<div className="row section-1-content">
+					<div className="col-xl-2 col-lg-3 col-sm-12 col-md-4 col-xs-12 offset-lg-1 offset-xl-2 align-self-center">
+						<div className="portfolio-photo">
+						</div>
+						{/* <img
 							className="img-fluid portfolio-photo"
 							src="images/me.jpeg"
 							alt="Failed"
-						/>
+						/> */}
 					</div>
-					<div className="col-xl-6 col-lg-7 col-sm-8 align-self-center">
-						<h1>Guðmundur Axel Guðrúnarson</h1>
-						<p>
+					<div className="col-xxl-5 col-xl-6 col-lg-7 col-md-7 col-sm-12 align-self-center">
+						<h2>Guðmundur Axel Guðrúnarson</h2>
+						<div className="portfolio-title-underline">
+
+						</div>
+						<p className="mt-3">
 							Hi!
 							<br />I have a lot of interest in Web Development and Game
 							Development. I have a lot of experience in both these fields from
@@ -38,7 +47,9 @@ function App() {
 							Always willing to learn and adapt to new technologies. Also have
 							experience with Machine Learning and Artificial Intelligence from
 							university. I am good at collaborating within teams and equally
-							proficient in independent work. Check out my projects below!
+							proficient in independent work. 
+							
+							<br/><br/>Check out my projects below!
 						</p>
 					</div>
 				</div>
